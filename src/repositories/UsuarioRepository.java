9f18316796caa847a476d66e27c5d7463138b979
Samuel Quaresma <squoliver@gmail.com>
2024-10-21 18:45:31 -0300

    Ajuste git

--- src/repositories/UsuarioRepository.java
+++ src/repositories/UsuarioRepository.java
@@ -8,6 +8,8 @@ import entities.usuario.Usuario;
 import utils.persistencia.LocadoraUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class UsuarioRepository implements Repositorio<Usuario, String> {
